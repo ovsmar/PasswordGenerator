3b94a8e27412b625756274fc5660b580af3dc8be
ovsmar <88561951+ovsmar@users.noreply.github.com>
2022-05-17 14:19:12 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+let copyButton = document.querySelector("#copyButton");
+
+function Submit() {
+  let mdpQNT = document.getElementById("qnt").value;
+  let letters = document.getElementById("letters").checked;
+  let nombres = document.getElementById("nombres").checked;
+  let speciaux = document.getElementById("speciaux").checked;
+  let password = "";
+
+  if (mdpQNT <= 100 && mdpQNT > 0) {
+    for (var i = 0; i < mdpQNT; i++) {
+      password = password + CheckBoxOfCharacter(letters, nombres, speciaux);
+      // console.log(password);
+    }
+    document.getElementById("password").innerHTML = password;
+  } else {
+    document.getElementById("password").innerHTML = "100 caractères maximum!";
+  }
+}
+
+function CheckBoxOfCharacter(letters, nombres, speciaux) {
+  let character = "";
+  if (letters === true) {
+    character =
+      character + "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (nombres === true) {
+    character = character + "1234567890";
+  }
+  if (speciaux === true) {
+    character = character + "!?@#$%^&*()[]-+^*=?^_`{|}~:";
+  }
+  character = character.charAt(Math.floor(Math.random() * character.length));
+  return character;
+}
+
+function copyMDP(e) {
+  e.preventDefault();
+  var password = document.querySelector("#password");
+
+  password.select();
+
+  document.execCommand("copy");
+}
+copyButton.addEventListener("click", copyMDP);
+
+// random bg color event click
+const body = document.querySelector("body");
+const btn = document.querySelector("#submit");
+const btn2 = document.querySelector("#copyButton");
+
+[btn, btn2].forEach((element) => {
+  element.addEventListener("click", () => {
+    let random1 = Math.floor(Math.random() * 255);
+    let random2 = Math.floor(Math.random() * 255);
+    let random3 = Math.floor(Math.random() * 255);
+
+    let colorValue = "rgb(" + random1 + "," + random2 + "," + random3 + ")";
+    body.style.background = colorValue;
+  });
+});
