d7d7c51dba5e906ca9e3249f2600b274b119c2d7
ovsmar <ovsep.simonain@gmail.com>
2022-07-28 22:31:51 +0200

    final

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ function Submit() {
   let speciaux = document.getElementById("speciaux").checked;
   let password = "";
 
+  let a = "💀";
+  let b = "🙂";
+  let c = "😀";
+  let d = "😁";
+  let e = "😈";
+  let f = "👹";
+  let g = "👽";
+  let h = "🤯";
+  let ii = "🤡";
+
+  let text = "Score de votre mot de passe :";
+
   if (mdpQNT <= 100 && mdpQNT > 0) {
     for (var i = 0; i < mdpQNT; i++) {
       password = password + CheckBoxOfCharacter(letters, nombres, speciaux);
@@ -15,6 +27,30 @@ function Submit() {
     document.getElementById("password").innerHTML = password;
   } else {
     document.getElementById("password").innerHTML = "100 caractères maximum!";
+  }
+
+  strong = document.getElementById("emoji");
+  score = document.getElementById("score");
+  score.innerHTML = text;
+
+  if (mdpQNT < 8) {
+    strong.innerHTML = a;
+  } else if (mdpQNT < 15) {
+    strong.innerHTML = b;
+  } else if (mdpQNT < 20) {
+    strong.innerHTML = c;
+  } else if (mdpQNT < 35) {
+    strong.innerHTML = d;
+  } else if (mdpQNT < 50) {
+    strong.innerHTML = e;
+  } else if (mdpQNT < 65) {
+    strong.innerHTML = f;
+  } else if (mdpQNT < 80) {
+    strong.innerHTML = g;
+  } else if (mdpQNT < 90) {
+    strong.innerHTML = h;
+  } else if (mdpQNT < 101) {
+    strong.innerHTML = ii;
   }
 }
 
@@ -30,7 +66,9 @@ function CheckBoxOfCharacter(letters, nombres, speciaux) {
   if (speciaux === true) {
     character = character + "!?@#$%^&*()[]-+^*=?^_`{|}~:";
   }
+
   character = character.charAt(Math.floor(Math.random() * character.length));
+
   return character;
 }
 
